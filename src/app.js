f2b7565f140920b6b79d9caba97c69970325948f
fhonoria <83973848+fhonoria@users.noreply.github.com>
2021-06-11 22:12:08 +0200

    set current date

--- src/app.js
+++ src/app.js
@@ -1,15 +1,41 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
+  console.log(response);
   let cityElement = document.querySelector("#city");
   let temperatureELement = document.querySelector("#degree");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#timestamp");
 
   cityElement.innerHTML = response.data.name;
   temperatureELement.innerHTML = Math.round(response.data.main.temp);
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = response.data.wind.speed;
+  dateElement.innerHTML = formatDate(response.data.dt);
 }
 
 let apiKey = "9840a840dcb1e2ca4cd13597387da153";
