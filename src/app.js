57fa536e9e884be500d57489eceed87baa40ce03
fhonoria <83973848+fhonoria@users.noreply.github.com>
2021-06-11 22:21:24 +0200

    added relevant icon from API

--- src/app.js
+++ src/app.js
@@ -22,13 +22,13 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-  console.log(response);
   let cityElement = document.querySelector("#city");
   let temperatureELement = document.querySelector("#degree");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#timestamp");
+  let iconElement = document.querySelector("#icon");
 
   cityElement.innerHTML = response.data.name;
   temperatureELement.innerHTML = Math.round(response.data.main.temp);
@@ -36,6 +36,11 @@ function displayTemperature(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = response.data.wind.speed;
   dateElement.innerHTML = formatDate(response.data.dt);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "9840a840dcb1e2ca4cd13597387da153";
